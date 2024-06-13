6074f7076b7a6fdc895b634abc2e5c042cd2e9c0
keby-dev <162151069+keby-dev@users.noreply.github.com>
2024-06-13 12:21:37 +0900

    commit test

---
+++ lv2/카펫.py
@@ -0,0 +1,13 @@
+# 완전 탐색(Brute Force)
+#   : 모든 경우의 수를 다 체크해서 정답을 찾는 방법
+#     1) 가능한 경우의 수를 대략적으로 계산
+#     2) 가능한 모든 방법을 다 고려
+#           - Brute Force 기법
+#           - 순열
+#           - 재귀 호출
+#           - 비트마스크(2진수 표현 기법 활용)
+#           - BFS, DFS를 활용
+
+def solution(brown, yellow):
+    answer = []
+    return answer
